738fdb08c519ffeeb8b7e13da6e1f5bc48ea37c0
Deeved Hiuston <deeved@gmail.com>
2020-06-06 12:32:43 -0300

    Reposição de alguns arquivos perdidos devido a reversão

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
 
 // configurar pasta publica
 server.use(express.static('public'))
+
+// habilitar o uso de req.body
+server.use(express.urlencoded({extended: true}))
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -24,8 +29,66 @@ server.get('/create-point', (req, res) => {
     return res.render('create-point.html')
 })
 
+server.post('/save-point', (req, res) => {
+
+    
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            adress,
+            adress2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.adress,
+        req.body.adress2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+
+    ]
+
+    function afterInsertData(err){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render('create-point.html', {saved: true})
+    }
+
+   db.run(query, values,afterInsertData)
+
+    
+})
+
 server.get('/search', (req, res) => {
-    return res.render('search-results.html')
+
+    const search = req.query.search
+   
+    if(search == "" ){
+        return res.render('search-results.html', {total:0})
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if(err) {
+            return console.log(err)
+        }
+        
+        const total = rows.length
+
+        return res.render('search-results.html', {places: rows, total})
+    })
+
 })
 
 //ligar o servidor
